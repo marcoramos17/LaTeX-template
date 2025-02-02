6faa55304185a0a466d931a95bba974e80a9180f
marcoramos17 <marcoandreramos17@gmail.com>
2025-02-02 17:53:58 +0000

    GitHub improvements. GitHub Pages set up.

---
+++ cleanup.bat
@@ -0,0 +1,4 @@
+@echo off
+echo Cleaning up auxiliary LaTeX files...
+del /S /Q *.aux *.bbl *.bcf *.blg *.lof *.log *.lol *.lot *.out *.synctex.gz *.toc *.fdb_latexmk *.fls *.run.xml
+echo Cleanup complete!
