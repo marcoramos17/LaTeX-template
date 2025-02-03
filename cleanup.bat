52db423a576c1186d10d2036fa88b7e94b043db8
marcoramos17 <marcoandreramos17@gmail.com>
2025-02-03 03:58:31 +0000

    Improvements to the overall document

--- cleanup.bat
+++ cleanup.bat
@@ -1,4 +1,4 @@
 @echo off
 echo Cleaning up auxiliary LaTeX files...
-del /S /Q *.aux *.bbl *.bcf *.blg *.lof *.log *.lol *.lot *.out *.synctex.gz *.toc *.fdb_latexmk *.fls *.run.xml
+del /S /Q *.aux *.bbl *.bcf *.blg *.lof *.log *.lol *.lot *.out *.synctex.gz *.toc *.fdb_latexmk *.fls *.run.xml *.aux *.bbl *.bcf *.blg *.lof *.log *.lol *.lot *.out *.synctex.gz *.toc *.fls *.fdb_latexmk *.run.xml *.acn *.acr *.alg *.glg *.ist *.glo *.gls
 echo Cleanup complete!
